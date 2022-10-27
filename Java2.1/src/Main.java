47bdffc3f7378179ce295668ff2373ea15400f33
TaisiaKol <tasia.pcma.le4@mail.ru>
2022-10-27 14:32:54 +0300

    count mails

---
+++ Java2.1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int prise = 100;
+        int oneMile = 20;
+        int mile = (prise / oneMile);
+        System.out.println(mile);
+    }
+}
