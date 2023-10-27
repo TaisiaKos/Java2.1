b428008fb100e0c13f506509a9f46fce76a37111
TaisiaKos <tasia.pcma.le4@mail.ru>
2023-10-27 17:17:44 +0300

    .gitignore

--- qafreelance/src/main/java/Freelancer.java
+++ qafreelance/src/main/java/Freelancer.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Freelancer {
+package ru.netology.service;
+
+public class Freelancer {
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money -= expenses;
+                money /= 3;
+                count++;
+            } else {
+                money = money + income;
+                money  -= expenses;
+            }
+        }
+        return count;
+    }
 }
